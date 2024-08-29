f0ddb2c3cec6ab4e46ebfa276ea7337ccee7b1b5
juansk8jaramillo <juansk8jaramillo@gmail.com>
2024-08-29 05:56:59 +0000

    add basic

--- praxis/main.js
+++ praxis/main.js
@@ -3,3 +3,14 @@ const variable = "mera vuelta"
 
 console.log(variable + 1)
 
+/*
+
+
+const | se puede declarra sin valor, no se puede reasignar, no se puede redeclarar 
+
+var | se puede declarar sin valor, se puede reasignar, se puede sedeclarar
+
+let | se puede declarar sin valor, se puede se reasignar, no se puede redeclarar 
+*/
+
+
