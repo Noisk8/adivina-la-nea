0b5e7c9ae80d31931bc020899194367a9777cd18
juansk8jaramillo <juansk8jaramillo@gmail.com>
2024-08-30 03:00:55 +0000

    add arrays

---
+++ praxis/arrray.js
@@ -0,0 +1,18 @@
+// un array o arreglo es una lista de objetos strings numbers booleanod
+
+//podemos acceder a el apartir del indice
+
+
+ const array = new Array (5)
+
+
+ array [0] ='hola'
+ array [1] ='como'
+ array [2] ='estas'
+ array [3] ='hoy'
+ array [4] ='hoy'
+
+
+document.getElementById('agente').innerHTML = array
+
+
