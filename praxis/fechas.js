6de4bb9c1c6abcafa12ebed06ef16e85ab26a9e4
juansk8jaramillo <juansk8jaramillo@gmail.com>
2024-09-03 01:35:33 +0000

    dates

---
+++ praxis/fechas.js
@@ -0,0 +1,29 @@
+const fecha = new Date()
+
+console.log(fecha)
+
+// fecha en especifico forma ISO 
+
+const fechas = new Date("2023-02-02")
+
+// SOLO QUIERO TRAER EL AÑO 
+
+const fecha2 = new Date()
+
+fecha2.setFullYear(2023)
+
+//como es un metodo se ponen () para que se ejecute
+console.log(fecha2.getFullYear())
+
+// para mostrar un mes en texto 
+
+let  meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
+let visaje = fecha2.getDay() + ' ' + meses [fecha2.getMonth()] + ' ' + fecha2.getFullYear()
+
+console.log(visaje)
+
+
+
+// set 
+
