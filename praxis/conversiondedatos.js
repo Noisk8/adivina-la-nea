3a3d802dc42043b9cdfd2b70daf490cc7e5cea33
juansk8jaramillo <juansk8jaramillo@gmail.com>
2024-09-05 03:16:18 +0000

    next

---
+++ praxis/conversiondedatos.js
@@ -0,0 +1,69 @@
+// let a = Number ('3.14')
+// let b = parseFloat('3.14')
+// let c = parseInt('2.12')
+// //convierte a number
+// let d = + '1'
+// console.log(a)
+// console.log(typeof a )
+
+
+// let a = Number ('1')
+
+
+// let b = String(1)  
+
+
+// console.log(b)
+// console.log(typeof b)
+
+
+// Number a string 
+
+// let a = Number ('1')
+// let b = String(1)  
+// let c = 1
+
+// let d = c.toString()
+
+// console.log(d)
+// console.log(typeof d)
+
+// Dates a number 
+
+// let e = new Date()
+// let f = Number(e)
+
+// console.log(f)
+// console.log(typeof f)
+
+
+// Dates a string 
+
+// let g = new Date()
+// let h = String(g)
+
+// console.log(h)
+// console.log(typeof h)
+
+
+// number  a boolean 
+// let i = Boolean(0)
+// let j = Boolean(i)
+
+// console.log(j)
+// console.log(typeof j)   
+
+let ricardo = 0 
+
+// condionar si se va rdcibir un 0 porque es 0 es false 
+if (ricardo && ricardo == 0){
+    console.log('Es verdadero')
+}else{
+    console.log('Es falso')
+}
+
+
+// https://www.w3schools.com/jsref/jsref_type_conversion.asp
+
+
+
