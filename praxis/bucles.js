767a1c93ba7af7a1a578b959fdd38d7d4a6933a4
juansk8jaramillo <juansk8jaramillo@gmail.com>
2024-09-05 02:00:29 +0000

    vamos que vamos

---
+++ praxis/bucles.js
@@ -0,0 +1,80 @@
+// Los bucles sirve o se usan generalmente para recorer arryas
+
+
+// let tecnologias = ['html','css','javascript', 'react']
+
+// for( tecnologia of tecnologias){
+//     console.log(tecnologia)
+// }
+
+
+
+// let texto = ''
+
+// for (let index = 0; index < tecnologias.length; index++) {
+// texto += tecnologias[index]
+// if (index < tecnologias.length -1) {
+//     texto += ', '
+// }
+//     seleccionable.textContent = texto
+    
+// }
+
+
+// Recorrer  objetos 
+
+// const alumno = {
+//     nombre: 'Ricardo',
+//     edad: 25,
+//     tecnologias: ['html', 'JS']
+// }
+
+// for (const clave in  alumno){
+//     console.log(alumno[clave])
+// }
+
+
+// let numeros = [1,2,3,4,5]
+
+//  numeros.forEach(numero => {
+//      console.log(numero)
+//  }) 
+
+
+
+// let edad = 0 
+
+// while (edad < 18 ){
+//     console.log(`Tienes ${edad} años y aun eres peque`)
+//     // hay que cerrar la condicion ↓↓
+//     edad ++
+// }
+
+// console.log(`Tienes ${edad} años y ya eres mayor de edad`)
+
+
+// let  edad = 0 
+
+// do{
+//     {console.log(`Tienes ${edad} años y aun eres peque`)
+//     // hay que cerrar la condicion ↓↓
+//     edad ++
+// }
+
+// }while (edad < 18)
+
+// console.log(`Tienes ${edad} años y ya eres mayor de edad`)
+
+
+
+
+let edad = 0
+
+
+while (edad < 30){
+    console.log(edad)
+    edad++;
+    if (edad == 25) break;
+}
+
+console.log(`saliste del bucle tienes ${edad} años`)
---
+++ praxis/setsymaps.js
@@ -0,0 +1,48 @@
+// let conjunto  = Set (['Pedro','Juan','Maria'])
+
+// Agregar 
+
+// conjunto.add('Consuelo')
+
+
+// conjunto.delete('Pedro')
+
+//  let pregunta = conjunto.has('Juan')
+
+//  console.log(pregunta)
+
+//  console.log(conjunto)
+
+
+// console.log(conjunto)
+
+
+
+// let array = ['Cama','Armario','Cama', 'Escritorio']
+
+// function comprar(muebles){
+//     console.log(muebles)
+// }
+
+
+// function comparrUnaSolaUnidad(muebles){
+    
+// let unicaUnidad = new Set(muebles)
+//     console.log(unicaUnidad)
+
+// }
+
+// comprar(array)
+
+
+
+// MAPS
+// son un conjunto pares de clave valor pero muy diferentes a los objetos, pero se usan para otro tipo de variables
+
+
+let map = new Map([
+
+    ['CPC', 30],
+    ['Tablet', 60],
+    ['Teclado', 50],
+])
